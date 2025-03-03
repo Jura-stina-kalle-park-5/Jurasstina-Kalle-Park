89aea301e74313e4a20f648daf5489db8ed2f3cd
Mathilda <mathildagrund@gmail.com>
2025-03-03 15:55:15 +0100

    Created BookSafariPage.py and added BookSafariPage variables. Deleted Safari variables from base and added import BookSafariPage

--- resources/variables_base.py
+++ resources/variables_base.py
@@ -1,4 +1,5 @@
 import os
+import BookSafariPage
 
 BROWSER = "chrome"
 current_directory = os.getcwd().replace('\\', '/')
@@ -36,28 +37,6 @@ register_error_no_username_password = "Please enter both a username and password
 register_error_username_already_exists = "Username already exists. Please choose another."
 register_success_message = "Registration successful! Redirecting to login..."
 register_button = "Register"
-
-# Book Safaris Page
-safari_section = "id:safari-section"
-safari_date_input = "id:safari-date"
-safari_past_date = "2021-01-01"
-safari_workday_date = "002026-01-01"
-safari_weekend_date = "002026-01-02"
-
-safari_type_select = "id:safari-type"
-safari_regular_option1 = "Herbivore Tour"
-safari_regular_option2 = "T-Rex Rumble"
-safari_vip_option1 = "Herbivore Tour with Feeding"
-safari_feeding_option = safari_vip_option1
-safari_vip_option2 = "T-Rex Rumble eXtreme Thrill Pack"
-
-safari_form = "id:safari-form"
-safari_message = "id:safari-message"
-safari_error_past_date = "You cannot book a safari for a date in the past."
-safari_error_no_ticket = "You must purchase a general admission ticket before booking a safari."
-safari_error_no_vip = "Only VIP users can book the selected safari option."
-safari_error_weekend_no_vip = "VIP tickets required to book safaris on weekends."
-safari_success_alert = "Item added to cart!"
 
 # Login Page
 login_section = "id:login-section"
