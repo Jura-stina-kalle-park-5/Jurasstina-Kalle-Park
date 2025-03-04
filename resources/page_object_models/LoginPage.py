c6f542c8cddd3c4eda1bcd41c4f15eda0af43026
kskolos <kskolos@cox.net>
2025-03-04 12:46:23 +0100

    Created the LoginPage and Register Page variable files, removed variables from base, moved .yml files to templates folder as instructed in labb document.

---
+++ resources/page_object_models/LoginPage.py
@@ -0,0 +1,15 @@
+# Page Link
+login_link = "data:section:login-section"
+
+# Page Navigation
+login_section = "id:login-section"
+login_username_input = "id:login-username"
+login_password_input = "id:login-password"
+login_form = "id:login-form"
+login_username_label = "for:login-username"  
+login_password_label = "for:login-password"  
+login_username_field = "id:login-username"  
+login_password_field = "id:login-password"  
+login_button = "Login"  
+login_message = "id:login-message"
+logout_link = "id:logout-link"
--- resources/variables_base.py
+++ resources/variables_base.py
@@ -3,7 +3,7 @@ import resources.page_object_models.CartPage as CartPage
 import resources.page_object_models.BookSafariPage as BookSafariPage
 
 BROWSER = "chrome"
-url = f"file:///${EXECDIR}/website/jurap.html"
+url = "file:///${EXECDIR}/website/jurap.html"
 
 # Login Entry
 username_with_numbers = '1234'
@@ -13,8 +13,6 @@ password_short = '1'
 password_long = 'p4$$w0rd'
 
 # Page Links
-register_link = "data:section:register-section"
-login_link = "data:section:login-section"
 safari_link = "data:section:safari-section"
 tickets_link = "data:section:tickets-section"
 cart_link = "data:section:cart-section"
@@ -25,18 +23,6 @@ home_page_text = "id:home-section"
 # Alert Message
 add_to_cart_success_alert = "Item added to cart!"
 checkout_success_alert = "Checkout Summary:"
-
-# Register Page
-register_section = "id:register-section"  # use with keyword Element Should Be Visible
-register_username_input = "id:reg-username"
-register_password_input = "id:reg-password"
-register_form = "id:register-form"  # use with keyword Submit Form
-register_message = "id:register-message"
-register_error_short_password = "Password must be at least 8 characters long."
-register_error_no_username_password = "Please enter both a username and password."
-register_error_username_already_exists = "Username already exists. Please choose another."
-register_success_message = "Registration successful! Redirecting to login..."
-register_button = "Register"
 
 # Book Safaris Page
 safari_section = "id:safari-section"
@@ -59,19 +45,6 @@ safari_error_no_ticket = "You must purchase a general admission ticket before bo
 safari_error_no_vip = "Only VIP users can book the selected safari option."
 safari_error_weekend_no_vip = "VIP tickets required to book safaris on weekends."
 safari_success_alert = "Item added to cart!"
-
-# Login Page
-login_section = "id:login-section"
-login_username_input = "id:login-username"
-login_password_input = "id:login-password"
-login_form = "id:login-form"
-login_username_label = "for:login-username"  
-login_password_label = "for:login-password"  
-login_username_field = "id:login-username"  
-login_password_field = "id:login-password"  
-login_button = "Login"  
-login_message = "id:login-message"
-logout_link = "id:logout-link"
 
 # Buy Tickets
 tickets_section = "id:tickets-section"
